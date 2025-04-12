fbb7d78f856589a26ab36691c89848126f623441
Ebrahim ElKordy <ebrahimelkordy@example.com>
2025-04-12 18:19:56 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/kordy-portfolio/', // تأكد من أن القيمة تطابق اسم المستودع
   plugins: [react()],
 })
