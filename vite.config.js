c76270e1ce8a1f48b98555109ba837a5c2be1d14
ebrahimelkordy <101756326+ebrahimelkordy@users.noreply.github.com>
2025-05-24 15:11:10 +0000

    تعديل على ملف

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  base: '/kordy-portfolio/', // تأكد من أن القيمة تطابق اسم المستودع
+  base: '/kordy-portfolio/',
   plugins: [react()],
-  resolve: {
-    extensions: ['.js', '.jsx'], // تأكد من دعم امتداد JSX
-  },
-})
+   resolve: {
+    extensions: ['.js', '.jsx'], 
+}});
